721af8ed0d84f86379ef3dabd42a76830d5e59f3
manubx1 <134285826+manubx1@users.noreply.github.com>
2023-11-16 18:30:56 -0300

    attLivros.sql

--- attLivros.sql
+++ attLivros.sql
@@ -17,25 +17,6 @@ CREATE TABLE emprestimos(
     FOREIGN KEY (livroID) REFERENCES livros (id_livro)
 ); 
 
-CREATE DATABASE attiv;
-
-USE attiv;
-
-CREATE TABLE livros(
-	id_livro INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(225) NOT NULL,
-    autor VARCHAR(225) NOT NULL,
-    quantidade_estoque INT NOT NULL
-);
-
-CREATE TABLE emprestimos(
-	id_emprestimo  INT AUTO_INCREMENT PRIMARY KEY,
-    data_emprestimo DATETIME,
-    data_devolucao DATETIME,
-	livroID INT, 
-    FOREIGN KEY (livroID) REFERENCES livros (id_livro)
-); 
-
 DELIMITER //
 
 CREATE TRIGGER tr_atualizar_emprestimo
